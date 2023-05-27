22830d20d236c680209850a5a154ce9d884c6881
CarlaJoha <carlajoha.work@gmail.com>
2023-05-26 20:46:26 -0400

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
-export const SERVICE_ID="service_upjgtqv";
-export const TEMPLATE_ID="template_0qh3akl";
-export const PUBLIC_KEY="z3syfHXO3tLppanmL"
+export const SERVICE_ID="x";
+export const TEMPLATE_ID="x";
+export const PUBLIC_KEY="x"
